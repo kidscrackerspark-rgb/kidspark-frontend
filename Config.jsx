1d70987f25086b1e34329f412bd595d544949bb8
kidscrackerspark-rgb <kidscrackerspark@gmail.com>
2025-12-11 17:17:42 +0530

    Config.jsx

--- Config.jsx
+++ Config.jsx
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'https://kidscrackerback.srimaheswarigroupofcompanies.in';
 // export const API_BASE_URL = 'http://localhost:5000';
-export const API_BASE_URL = 'https://kidspark.onrender.com'
+export const API_BASE_URL = 'https://kidscrackerparkb.onrender.com'
